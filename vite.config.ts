900889714e5b9ade7b339c1296c18b5e1d3ee3f3
Subhajit Satpathi <subhajitsatpathi6@gmail.com>
2026-09-14 20:56:48 +0530

    Bundle compatible PDF worker as ES module

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: './',
   build: { target: 'chrome70' },
+  worker: { format: 'es' },
   plugins: [react()],
   server: {
     host: "0.0.0.0",
