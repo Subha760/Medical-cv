8bbb8fa69567d13a4b58109b1da98ab3b2a24d1e
Subhajit Satpathi <subhajitsatpathi6@gmail.com>
2026-09-14 21:10:59 +0530

    Use classic PDF worker on Android 10 WebView

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const workerCompatibility = 'if(!Array.prototype.at)Object.defineProperty(Array.prototype,"at",{value:function(i){return this[i<0?this.length+i:i]},configurable:true,writable:true});if(!String.prototype.at)Object.defineProperty(String.prototype,"at",{value:function(i){i=i<0?this.length+i:i;return this.charAt(i)||void 0},configurable:true,writable:true});';
+
 export default defineConfig({
   base: './',
   build: { target: 'chrome70' },
-  worker: { format: 'es', rollupOptions: { output: { banner: "if(!Array.prototype.at)Object.defineProperty(Array.prototype,\"at\",{value:function(i){return this[i<0?this.length+i:i]},configurable:true,writable:true});if(!String.prototype.at)Object.defineProperty(String.prototype,\"at\",{value:function(i){i=i<0?this.length+i:i;return this.charAt(i)||void 0},configurable:true,writable:true});" } } },
+  worker: { rollupOptions: { output: { banner: workerCompatibility } } },
   plugins: [react()],
   server: {
     host: "0.0.0.0",
